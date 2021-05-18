5d69d9b9fa52ddcab12cbe27f4af8c4e4a9a7a3e
Bastien de Luca <dev@de-luca.io>
2021-05-18 17:47:00 +0200

    Add Beacon class and tests

---
+++ src/model/net/Beacon.spec.ts
@@ -0,0 +1,112 @@
+import { Beacon } from './Beacon';
+import WS from "jest-websocket-mock";
+
+let srv: WS;
+
+beforeEach(() => {
+    srv = new WS("ws://localhost:3030", { jsonProtocol: true });
+});
+
+afterEach(() => {
+    srv.close();
+})
+
+// test("the mock server sends messages to connected clients", async () => {
+//     const server = new WS("ws://localhost:1234");
+//     const client1 = new WebSocket("ws://localhost:1234");
+//     await server.connected;
+//     const client2 = new WebSocket("ws://localhost:1234");
+//     await server.connected;
+
+//     const messages = { client1: [] as Array<any>, client2: [] as Array<any> };
+//     client1.onmessage = (e) => {
+//         messages.client1.push(e.data);
+//     };
+//     client2.onmessage = (e) => {
+//         messages.client2.push(e.data);
+//     };
+
+//     server.send("hello everyone");
+//     expect(messages).toEqual({
+//         client1: ["hello everyone"],
+//         client2: ["hello everyone"],
+//     });
+// });
+
+describe('Beacon.constructor', () => {
+    it('starts a websocket and emit ready event', async() => {
+        const beacon = await new Promise<Beacon>((resolve) => {
+            const beacon = new Beacon();
+            beacon.addEventListener('ready', (_ev) => {
+                resolve(beacon);
+            });
+        }); 
+        expect(beacon['socket'].readyState).toEqual(1);
+    });
+});
+
+describe('Beacon.send', () => {
+    it('sends a create/join/signal payload', async () => {
+        const beacon = new Beacon();
+        await srv.connected;
+
+        beacon.create();
+        await expect(srv).toReceiveMessage({
+            method: 'create',
+            params: {},
+        });
+
+        beacon.join('00000000-0000-0000-0000-000000000000');
+        await expect(srv).toReceiveMessage({
+            method: 'join',
+            params: { roomId: '00000000-0000-0000-0000-000000000000' },
+        });
+
+        beacon.signal('00000000-0000-0000-0000-000000000000', { foo: 'bar' });
+        await expect(srv).toReceiveMessage({
+            method: 'signal',
+            params: {
+                peerId: '00000000-0000-0000-0000-000000000000',
+                data: { foo: 'bar' },
+            },
+        });
+    });
+});
+
+
+describe('Beacon.handle', () => {
+    it('receives events', async () => {
+        const beacon = new Beacon();
+        await srv.connected;
+
+        const createdData = new Promise((resolve) => {
+            beacon.addEventListener('created', (ev) => {
+                resolve((ev as CustomEvent).detail);
+            });
+        });
+        srv.send({ event: 'created', data: '00000000-0000-0000-0000-000000000000' });
+        expect(await createdData).toEqual('00000000-0000-0000-0000-000000000000');
+
+        const joinedData = new Promise((resolve) => {
+            beacon.addEventListener('joined', (ev) => {
+                resolve((ev as CustomEvent).detail);
+            });
+        });
+        srv.send({ event: 'joined', data: ['00000000-0000-0000-0000-000000000000'] });
+        expect(await joinedData).toEqual(['00000000-0000-0000-0000-000000000000']);
+
+        const signalData = new Promise((resolve) => {
+            beacon.addEventListener('signal', (ev) => {
+                resolve((ev as CustomEvent).detail);
+            });
+        });
+        srv.send({ event: 'signal', data: {
+            peerId: '00000000-0000-0000-0000-000000000000',
+            data: { foo: 'bar' },
+        }});
+        expect(await signalData).toEqual({
+            peerId: '00000000-0000-0000-0000-000000000000',
+            data: { foo: 'bar' },
+        });
+    });
+});
---
+++ src/model/net/Beacon.ts
@@ -0,0 +1,79 @@
+enum Method {
+    CREATE = 'create',
+    JOIN = 'join',
+    SIGNAL = 'signal',
+}
+
+enum EventType {
+    READY = 'ready',
+    CREATED = 'created',
+    JOINED = 'joined',
+    SIGNAL = 'signal',
+}
+
+interface Response<T> {
+    event: EventType;
+    data: T
+}
+
+interface SignalData {
+    peerId: string;
+    data: Record<string, unknown>;
+}
+
+type Param = {} | { roomId: string } | SignalData;
+
+export class Beacon extends EventTarget {
+    private socket: WebSocket;
+
+    public constructor() {
+        super();
+
+        this.socket = new WebSocket('ws://localhost:3030');        
+        this.socket.onopen = () => this.open();
+    }
+
+    public create(): void {
+        this.send(Method.CREATE);
+    }
+
+    public join(roomId: string): void {
+        this.send(Method.JOIN, { roomId });
+    }
+
+    public signal(peerId: string, data: Record<string, unknown>): void {
+        this.send(Method.SIGNAL, { peerId, data });
+    }
+
+    private send(method: Method, params: Param = {}): void {
+        this.socket.send(
+            JSON.stringify({ method, params }),
+        );
+    }
+
+    private open(): void {
+        this.socket.onmessage = (ev) => this.handle(ev);
+        this.dispatchEvent(new Event(EventType.READY));
+    }
+
+    private handle(event: MessageEvent<string>): void {
+        const data = JSON.parse(event.data) as Response<any>;
+        switch(data.event) {
+            case EventType.CREATED:
+                this.dispatchEvent(new CustomEvent<string>(EventType.CREATED, {
+                    detail: data.data as string,
+                }));
+                break;
+            case EventType.JOINED:
+                this.dispatchEvent(new CustomEvent<string>(EventType.JOINED, {
+                    detail: data.data as string,
+                }));
+                break;
+            case EventType.SIGNAL:
+                this.dispatchEvent(new CustomEvent<SignalData>(EventType.SIGNAL, {
+                    detail: data.data as SignalData,
+                }));
+                break;
+        }
+    }
+}
