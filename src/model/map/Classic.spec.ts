28cd3ee9c8c4e7a250037cc43d19af7e6d26ea32
Bastien de Luca <dev@de-luca.io>
2021-06-30 09:39:49 +0200

    Fix Classic map tests

--- src/model/map/Classic.spec.ts
+++ src/model/map/Classic.spec.ts
@@ -2,6 +2,17 @@ import Container from 'typedi';
 import { DeckProvider } from '@/services/DeckProvider';
 import { Classic } from '.';
 import { Plane } from '../card';
+import { MapType } from './MapInterface';
+
+describe('Classic.type', () => {
+  it('returns the type', () => {
+    const map = new Classic({
+      deck: Container.get(DeckProvider).getDeck(),
+    });
+
+    expect(map.type).toEqual(MapType.CLASSIC);
+  });
+});
 
 describe('Classic.drawPlane', () => {
   it('draws a plane', () => {
@@ -9,8 +20,11 @@ describe('Classic.drawPlane', () => {
       deck: Container.get(DeckProvider).getDeck(),
     });
 
-    expect(map['drawPlane']()).toBeInstanceOf(Plane);
-    expect(map['deck']).toHaveLength(84);
+    const drawn = map['drawPlane']();
+
+    expect(drawn.card).toBeInstanceOf(Plane);
+    expect(drawn.shuffled).toEqual(false);
+    expect(map.deck).toHaveLength(84);
   });
 });
 
@@ -19,14 +33,34 @@ describe('Classic.planeswalk', () => {
     const map = new Classic({
       deck: Container.get(DeckProvider).getDeck(),
     });
-    const startCard = map['active'];
-    const deckSize = map['deck'].length;
-    const playedSize = map['played'].length;
+    const startCard = map.active;
+    const deckSize = map.deck.length;
+    const playedSize = map.played.length;
 
     map.planeswalk();
 
-    expect(map['active']).not.toEqual(startCard);
-    expect(map['deck'].length).toBeLessThan(deckSize);
-    expect(map['played'].length).toBeGreaterThan(playedSize);
+    expect(map.active).not.toEqual(startCard);
+    expect(map.deck.length).toBeLessThan(deckSize);
+    expect(map.played.length).toBeGreaterThan(playedSize);
+  });
+});
+
+describe('Classic.customPlaneswalk', () => {
+  it('changes active card to given planes', () => {
+    const map = new Classic({
+      deck: Container.get(DeckProvider).getDeck(),
+    });
+    const startCard = map.active;
+    const deckSize = map.deck.length;
+    const playedSize = map.played.length;
+
+    map.revealUntil(2, Plane);
+    map.customPlaneswalk(map.revealed?.relevant as Array<Plane>);
+    map.resolveReveal([], map.revealed?.others ?? []);
+
+    expect(map.active).not.toEqual(startCard);
+    expect(map.active).toHaveLength(2);
+    expect(map.deck.length).toBeLessThan(deckSize);
+    expect(map.played.length).toBeGreaterThan(playedSize);
   });
 });
