8236567b909e4fb94919b490e1efc3b188b7687d
Bastien de Luca <dev@de-luca.io>
2021-08-18 21:09:52 +0200

    Add random maze background on home

---
+++ src/components/Maze.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="maze" id="canary" aria-hidden="true" v-html="glyphs"></div>
+  <div class="maze" aria-hidden="true" v-html="pattern"></div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+export default class Maze extends Vue {
+  private static readonly rows = 10;
+
+  private glyphs: string = '';
+  private cols: number = 0;
+
+  public created(): void {
+    this.glyphs = Math.random().toString(36).substring(2, 6);
+    this.$nextTick(this.computeCols);
+    window.addEventListener("resize", this.computeCols);
+  }
+
+  public unmounted(): void {
+    window.removeEventListener("resize", this.computeCols);
+  }
+
+  private computeCols(): void {
+    const canaryWidth = (document.getElementById('canary') as HTMLElement).clientWidth;
+    const screenWidth = window.innerWidth;
+    console.log(canaryWidth);
+    console.log(screenWidth);
+
+    this.cols = Math.ceil(screenWidth / canaryWidth);
+  }
+
+  public get pattern(): string {
+    let pattern = '';
+    for (let r = 0; r < Maze.rows; r++) {
+      for(let c = 0; c < this.cols; c++) {
+        pattern += this.glyphs;
+      }
+      pattern += '<br>';
+    }
+
+    return pattern;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.maze {
+  font-family: 'MazeletterUtopia';
+  color: #f9f9f9;
+  display: block;
+  position: fixed;
+  z-index: 0;
+  top: 0;
+  left: 0;
+  margin: 0;
+  padding: 0;
+  text-align: left;
+  font-size: 10vh;
+  line-height: 1em;
+  overflow: hidden;
+  white-space: nowrap;
+  pointer-events: none;
+}
+
+#canary {
+  position: absolute;
+  visibility: hidden;
+  height: auto;
+  width: auto;
+  white-space: nowrap;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,15 @@
 <template>
+  <maze />
   <div class="container">
     <div class="cta">
-      <h1 class="title">Mirrod.in</h1>
+      <h1 class="title">
+        <div class="brand" @click="thaNav">
+          <div>⟁</div>
+          <div>⟁</div>
+          <div>⟁</div>
+        </div>
+        Mirrod.in
+      </h1>
       <div class="btn-grp">
         <router-link class="button is-light" to="/join">
           Join game
@@ -15,8 +23,10 @@
 </template>
 
 <script lang="ts">
-import { Vue } from 'vue-class-component';
+import { Options, Vue } from 'vue-class-component';
+import Maze from '@/components/Maze.vue';
 
+@Options({ components: { Maze } })
 export default class Home extends Vue {}
 </script>
 
@@ -39,6 +49,33 @@ export default class Home extends Vue {}
 
       .button {
         width: 150px;
+      }
+    }
+
+    .brand {
+      text-align: center;
+
+      div {
+        position: relative;
+        z-index: 1;
+        display: inline-block;
+        width: 3rem;
+        text-align: center;
+
+        &:hover {
+          cursor: pointer;
+          color: gray;
+        }
+
+        &:nth-child(1) {
+          transform: rotate(180deg) translateX(-1.5rem);
+        }
+        &:nth-child(2) {
+          z-index: 2;
+        }
+        &:nth-child(3) {
+          transform: rotate(180deg) translateX(1.5rem);
+        }
       }
     }
   }
