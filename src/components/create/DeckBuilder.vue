a47d90ff879e71976517b73a1cb49be666f99702
Bastien de Luca <dev@de-luca.io>
2022-02-03 22:01:24 +0100

    Make deck builder responsive (kinda)

--- src/components/create/DeckBuilder.vue
+++ src/components/create/DeckBuilder.vue
@@ -2,7 +2,39 @@
   <div class="modal" style="display: block">
     <div class="modal-background"></div>
     <div class="modal-content">
+
       <h1 class="title">Deck Customization</h1>
+
+      <aside class="menu">
+        <ul class="menu-list">
+          <li>
+            <a
+              v-if="scope === 'all'"
+              @click="selectedGroup = 'all'"
+              :class="{'is-active': selectedGroup === 'all'}"
+            >All cards</a>
+          </li>
+          <li>
+            <a
+              @click="selectedGroup = 'plane'"
+              :class="{'is-active': selectedGroup === 'plane'}"
+            >Planes</a>
+          </li>
+          <li>
+            <a
+              v-if="scope === 'all'"
+              @click="selectedGroup = 'phenomenon'"
+              :class="{'is-active': selectedGroup === 'phenomenon'}"
+            >Phenomenon</a>
+          </li>
+          <li>
+            <a
+              @click="selectedGroup = 'decks'"
+              :class="{'is-active': selectedGroup === 'decks'}"
+            >Decks</a>
+          </li>
+        </ul>
+      </aside>
 
       <div class="panel">
         <p class="panel-tabs" >
@@ -65,10 +97,18 @@
         </template>
       </div>
 
-      <div class="field">
+      <div class="field use">
         <div class="control">
           <button class="button is-fullwidth is-secondary" @click.prevent="done">
             Use this Deck
+          </button>
+        </div>
+      </div>
+
+      <div class="field close">
+        <div class="control">
+          <button class="button is-fullwidth is-secondary" @click.prevent="done">
+            Close builder
           </button>
         </div>
       </div>
@@ -161,18 +201,59 @@ export default class DeckBuilder extends Vue.with(Props) {
 
 <style lang="scss" scoped>
 .modal-content {
+  @media screen and (max-height: 450px) and (orientation: landscape) {
+    max-width: 100vw;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    background-color: #ffffff;
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-template-rows: 1fr auto auto;
+    row-gap: .5rem;
+    grid-template-areas:
+      "menu  builder"
+      "close builder"
+      "use   builder"
+    ;
+
+    h1.title { display: none };
+  }
+
   max-width: 800px;
   max-height: 100vh;
   height: 100%;
+
   display: flex;
   flex-direction: column;
   justify-content: center;
+
   padding: .5rem;
   margin: 0 auto;
 
   h1.title {
     text-align: center;
     color: whitesmoke;
+  }
+}
+
+.menu {
+  @media screen and (max-height: 450px) and (orientation: landscape) {
+    grid-area: menu;
+    display: block;
+  }
+
+  display: none;
+
+  background-color: var(--bg-color);
+
+  .menu-list a {
+    color: var(--text-color);
+    border-radius: 0;
+
+    &:hover, &.is-active {
+      background-color: var(--secondary);
+    }
   }
 }
 
@@ -192,6 +273,16 @@ export default class DeckBuilder extends Vue.with(Props) {
 }
 
 .panel {
+  @media screen and (max-height: 450px) and (orientation: landscape) {
+    grid-area: builder;
+    border-radius: 0;
+    border-top: none;
+    border-bottom: none;
+    border-right: none;
+    margin: 0;
+    box-shadow: none;
+  }
+
   background-color: var(--bg-color);
   border: 1px solid var(--border-color);
 
@@ -200,6 +291,10 @@ export default class DeckBuilder extends Vue.with(Props) {
   }
 
   .panel-tabs {
+    @media screen and (max-height: 450px) and (orientation: landscape) {
+      display: none;
+    }
+
     font-size: 1em;
     padding-left: 1rem;
     padding-right: 1rem;
@@ -240,9 +335,40 @@ export default class DeckBuilder extends Vue.with(Props) {
   }
 
   .card-list {
+    @media screen and (max-height: 450px) and (orientation: landscape) {
+      height: calc(100vh - 7em - 2px);
+    }
+
     height: 60vh;
     overflow: scroll;
     border-bottom: 1px solid var(--border-color);
+  }
+}
+
+.field {
+  margin: 0;
+  padding: 0 .5rem;
+
+  &.use {
+    @media screen and (max-height: 450px) and (orientation: landscape) {
+      grid-area: use;
+      padding-bottom: .5rem;
+    }
+  }
+
+  &.close {
+    display: none;
+
+    @media screen and (max-height: 450px) and (orientation: landscape) {
+      display: block;
+      grid-area: close;
+    }
+  }
+}
+
+.modal-close {
+  @media screen and (max-height: 450px) and (orientation: landscape) {
+    display: none;
   }
 }
 </style>
