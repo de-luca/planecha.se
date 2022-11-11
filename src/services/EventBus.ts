057dadfc8fa16be5b02cd085cc8358e4cee14780
Bastien de Luca <dev@de-luca.io>
2022-11-11 16:44:20 +0100

    Upgrade mitt to 3.0.0

--- src/services/EventBus.ts
+++ src/services/EventBus.ts
@@ -1,4 +1,4 @@
-import mitt, { EventHandlerMap } from 'mitt';
+import mitt from 'mitt';
 
 export enum EventType {
   ARETOPOLIS = 'ARETOPOLIS',
@@ -9,13 +9,6 @@ export enum EventType {
   SPACIAL_MERGING = 'SPACIAL_MERGING',
 }
 
-type Emits<EventType, T> = {
-  all: EventHandlerMap;
-  on(type: EventType, handler: (arg: T) => void): void;
-  off(type: EventType, handler: (arg: T) => void): void;
-  emit(type: EventType, arg?: T): void;
-};
+type Events = Record<EventType, void>;
 
-type Emitter = Emits<EventType, undefined>;
-
-export const eventBus: Emitter = mitt();
+export const eventBus = mitt<Events>();
